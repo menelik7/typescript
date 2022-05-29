ca16328b799311ab63d9a933b1acc8a426988586
Menelik Tefera <menelikworku@gmail.com>
2022-05-29 13:57:25 +0200

    Initial commit

---
+++ stats/src/MatchResult.ts
@@ -0,0 +1,6 @@
+// enum - enumeration - to use only when values are known, unchanging, and limited (10 to 15 at most)
+export enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
